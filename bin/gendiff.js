4e5f1a89a2a644b7e70767f0843df942bfb81218
pkryazhev <kryazhevpavel94@gmail.com>
2026-05-14 15:20:46 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,8 @@ const { program } = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .version('0.0.1');
 
 program.parse();
