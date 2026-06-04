f648ab1ac69a4d65861d9a0b9570c9150a2b48d2
pkryazhev <kryazhevpavel94@gmail.com>
2026-06-04 18:15:21 +0300

    step 7

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 import { program } from 'commander'
 import { readFileSync } from 'node:fs'
 import path from 'node:path'
-import { compare, parseFile } from '../src/parser.js'
+import { compare, parseFile, format } from '../src/parser.js'
 
 program
   .description('Compares two configuration files and shows a difference.')
@@ -10,10 +10,13 @@ program
   .version('0.0.1')
   .action((filepath1, filepath2) => {
     const path1 = path.resolve(filepath1)
+    const ext1 = path.extname(filepath1)
     const path2 = path.resolve(filepath2)
-    const fileData1 = readFileSync(path1)
-    const fileData2 = readFileSync(path2)
-    console.log(compare(parseFile(fileData1), parseFile(fileData2)))
+    const ext2 = path.extname(filepath2)
+    const fileData1 = readFileSync(path1, 'utf-8')
+    const fileData2 = readFileSync(path2, 'utf-8')
+    const data = compare(parseFile(fileData1, ext1), parseFile(fileData2, ext2))
+    console.log(format(data));
   })
 
 program.parse()
--- src/parser.js
+++ src/parser.js
@@ -1,30 +1,122 @@
 import _ from 'lodash'
+import { parse } from 'yaml'
 
-export const parseFile = function (fileData) {
-  return JSON.parse(fileData)
+export const parseFile = function (fileData, ext) {
+  switch (ext) {
+    case '.json':
+      return JSON.parse(fileData)
+    case '.yml':
+    case '.yaml':
+      return parse(fileData)
+    default:
+      throw new Error('Неизвестный формат файла')
+  }
 }
 
-export const compare = function (object1, object2) {
-  const result = []
-  const keys = _.union(Object.keys(object1), Object.keys(object2)).sort()
-  for (const key of keys) {
-    if (Object.prototype.hasOwnProperty.call(object2, key) && Object.prototype.hasOwnProperty.call(object1, key)) {
-      if (object1[key] === object2[key]) {
-        result.push(`   ${key}: ${object1[key]}`)
-      }
-      else {
-        result.push(` - ${key}: ${object1[key]}`)
-        result.push(` + ${key}: ${object2[key]}`)
-      }
+const isObject = (value) => {
+  return value !== null && typeof value === 'object'
+}
+
+
+const getNode = (key, object1, object2) => {
+  const value1 = object1[key];
+  const value2 = object2[key];
+  if (!Object.hasOwn(object1, key)) {
+    return {
+      key,
+      type: "added",
+      value: value2
+    }
+  }
+  if (!Object.hasOwn(object2, key)) {
+    return {
+      key,
+      type: "removed",
+      value: value1
+    }
+  }
+  if (isObject(value1) && isObject(value2)) {
+    return {
+      key,
+      type: "nested",
+      children: compare(value1, value2)
+    }
+  }
+  else {
+    if (value1 === value2) {
+      return {
+        key,
+        type: "unchanged",
+        value: value1
+    }
     }
     else {
-      if (Object.prototype.hasOwnProperty.call(object1, key)) {
-        result.push(` - ${key}: ${object1[key]}`)
-      }
-      else {
-        result.push(` + ${key}: ${object2[key]}`)
+      return {
+        key,
+        type: "changed",
+        oldValue: value1,
+        newValue: value2
       }
     }
   }
-  return '{\n' + result.join('\n') + '\n}'
+}
+
+const getIndent = (depth) => {
+  return "    ".repeat(depth);
+}
+
+const stringify = (value, depth) => {
+  if (!isObject(value)) {
+    return String(value);
+  }
+  const keys = Object.keys(value);
+  const result = keys.map((key) => {
+    if(isObject(value[key])) {
+      return `${getIndent(depth + 1)}${key}: ${stringify(value[key], depth + 1)}`;
+    }
+    return `${getIndent(depth + 1)}${key}: ${value[key]}`;
+  });
+  return `{\n${result.join("\n")}\n${getIndent(depth)}}`;
+}
+
+const formatTree = (tree, depth) => {
+  return tree.map((node) => formatNode(node, depth)).join('\n');
+};
+
+const formatNode = (node, depth) => {
+  switch (node.type) {
+    case "added" : {
+      return `${getIndent(depth)}  + ${node.key}: ${stringify(node.value, depth + 1)}`
+    }
+    case "removed" : {
+      return `${getIndent(depth)}  - ${node.key}: ${stringify(node.value, depth + 1)}`
+    }
+    case "unchanged" : {
+      return `${getIndent(depth)}    ${node.key}: ${stringify(node.value, depth + 1)}`
+    }
+    case "changed" : {
+      return `${getIndent(depth)}  - ${node.key}: ${stringify(node.oldValue, depth + 1)}\n${getIndent(depth)}  + ${node.key}: ${stringify(node.newValue, depth + 1)}`
+    }
+    case "nested" : {
+      return `${getIndent(depth + 1)}${node.key}: {\n${formatTree(node.children, depth + 1)}\n${getIndent(depth + 1)}}`;
+    }
+    default: {
+      return "Error"
+    }
+  }
+}
+
+export const compare = function (object1, object2) {
+  const keys = _.union(Object.keys(object1), Object.keys(object2)).sort()
+  const result = keys.map((key) => {
+    return getNode(key, object1, object2);
+  })
+  return result;
+}
+
+export const format = function (data) {
+  const result = data.map((node) => {
+    return formatNode(node, 0);
+  })
+  return "{\n" + result.join("\n") + "\n}";
 }
